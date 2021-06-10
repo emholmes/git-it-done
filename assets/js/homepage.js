8460c36b05e8a8315461092b7f5dbe05209b06eb
Erin Holmes <e.holmes09@yahoo.com>
2021-06-10 18:37:23 -0500

    add topic buttons and logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEL = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -48,9 +49,8 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name; 
 
         // create a container for each repo
-        var repoEl = document.createElement("a");
+        var repoEl = document.createElement("div");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
-        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         //create a span element to hold repo name
         var titleEl = document.createElement("span");
@@ -77,4 +77,31 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language)
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+}
+
+var buttonClickHanler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if (language) {
+        getFeaturedRepos(language);
+
+        repoContainerEl.textContent = "";
+    }
+    
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEL.addEventListener("click", buttonClickHanler);
